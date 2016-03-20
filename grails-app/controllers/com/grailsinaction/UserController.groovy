ad887974fbfe48bb033109bbf148f3039bce655b
Igor Shkliar <igorsh@default-value.com>
2016-03-20 22:53:52 +0200

    Chapter 05. Add search and advSearch actions and views

--- grails-app/controllers/com/grailsinaction/UserController.groovy
+++ grails-app/controllers/com/grailsinaction/UserController.groovy
@@ -10,6 +10,36 @@ class UserController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
+    def search() {}
+
+    def results(String loginId) {
+        def users = User.where { loginId =~ "%${loginId}%" }.list()
+        return [ users: users,
+                 term: params.loginId,
+                 totalUsers: User.count() ]
+    }
+
+    def advSearch() {}
+
+    def advResults() {
+        def profileProps = Profile.metaClass.properties*.name
+        def profiles = Profile.withCriteria {
+            "${params.queryType}" {
+
+                params.each { field, value ->
+
+                    if (profileProps.grep(field) && value) {
+                        ilike(field, value)
+                    }
+                }
+
+            }
+
+        }
+        [ profiles : profiles ]
+
+    }
+
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond User.list(params), model:[userInstanceCount: User.count()]
