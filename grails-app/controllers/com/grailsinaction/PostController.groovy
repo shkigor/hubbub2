d9ffce6ccf0d5b63d6c91f786dd42d928620fedd
Igor Shkliar <igorsh@default-value.com>
2016-03-22 00:04:26 +0200

    Chapter 06. Controlling application flow. Add timeline action to PostController, add view and unit test

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -2,4 +2,39 @@ package com.grailsinaction
 
 class PostController {
     static scaffold = true
+
+    static defaultAction = "home"
+
+    def home() {
+        if (!params.id) {
+            params.id = "chuck_norris"
+        }
+        redirect(action: 'timeline', params: params)
+    }
+    
+    def timeline() {
+        def user = User.findByLoginId(params.id)
+        if (!user) {
+            response.sendError(404)
+        } else {
+            [ user : user ]
+        }
+    }
+
+    def addPost() {
+        def user = User.findByLoginId(params.id)
+        if (user) {
+            def post = new Post(params)
+            user.addToPosts(post)
+            if (user.save(flush: true)) {
+                flash.message = "Successfully created Post"
+            } else {
+                flash.message = "Invalid or empty post"
+            }
+        } else {
+            flash.message = "Invalid User Id"
+        }
+        redirect(action: 'timeline', id: params.id)
+    }
+    
 }
--- test/unit/com/grailsinaction/PostControllerSpec.groovy
+++ test/unit/com/grailsinaction/PostControllerSpec.groovy
@@ -1,20 +1,105 @@
 package com.grailsinaction
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(PostController)
+@Mock([User, Post])
 class PostControllerSpec extends Specification {
 
-    def setup() {
+    def "Get a users timeline given their id"() {
+        given: "A user with posts in the db"
+        User chuck = new User(
+                loginId: "chuck_norris",
+                password: "password")
+        chuck.addToPosts(new Post(content: "A first post"))
+        chuck.addToPosts(new Post(content: "A second post"))
+        chuck.save(failOnError: true)
+
+        and: "A loginId parameter"
+        params.id = chuck.loginId
+
+        when: "the timeline is invoked"
+        def model = controller.timeline()
+
+        then: "the user is in the returned model"
+        model.user.loginId == "chuck_norris"
+        model.user.posts.size() == 2
     }
 
-    def cleanup() {
+    def "Check that non-existent users are handled with an error"() {
+
+        given: "the id of a non-existent user"
+        params.id = "this-user-id-does-not-exist"
+
+        when: "the timeline is invoked"
+        controller.timeline()
+
+        then: "a 404 is sent to the browser"
+        response.status == 404
+
     }
 
-    void "test something"() {
+    def "Adding a valid new post to the timeline"() {
+        given: "A user with posts in the db"
+        User chuck = new User(
+                loginId: "chuck_norris",
+                password: "password").save(failOnError: true)
+
+        and: "A loginId parameter"
+        params.id = chuck.loginId
+
+        and: "Some content for the post"
+        params.content = "Chuck Norris can unit test entire applications with a single assert."
+
+        when: "addPost is invoked"
+        def model = controller.addPost()
+
+        then: "our flash message and redirect confirms the success"
+        flash.message == "Successfully created Post"
+        response.redirectedUrl == "/post/timeline/${chuck.loginId}"
+        Post.countByUser(chuck) == 1
+
     }
+
+    def "Adding an invalid new post to the timeline"() {
+        given: "A user with posts in the db"
+        User chuck = new User(loginId: "chuck_norris", password: "password").save(failOnError: true)
+
+        and: "A loginId parameter"
+        params.id = chuck.loginId
+
+        and: "Some content for the post"
+        params.content = null
+
+        when: "addPost is invoked"
+        def model = controller.addPost()
+
+        then: "our flash message and redirect confirms the success"
+        flash.message == "Invalid or empty post"
+        response.redirectedUrl == "/post/timeline/${chuck.loginId}"
+        Post.countByUser(chuck) == 0
+    }
+
+    @Unroll                                             
+    def "Testing id of #suppliedId redirects to #expectedUrl"() {  
+                                                                   
+        given:                                                     
+        params.id = suppliedId                                     
+                                                                   
+        when: "Controller is invoked"                              
+        controller.home()                                         
+                                                                   
+        then:                                                      
+        response.redirectedUrl == expectedUrl                      
+                                                                   
+        where:                                                     
+        suppliedId  |   expectedUrl                                
+        'joe_cool'  |   '/post/timeline/joe_cool'                  
+        null        |   '/post/timeline/chuck_norris'              
+                                                                   
+    }
+
 }
