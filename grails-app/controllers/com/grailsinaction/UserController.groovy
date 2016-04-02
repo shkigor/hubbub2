59a2972aa8285bcb46ba75f0449c593e82435758
Igor Shklyar <igor.shklyar@gmail.com>
2016-04-02 21:14:42 +0300

    Chapter 07. Data binding. Binding multiply objects. Add register action and view. Add Command object UserRegistrationCommand. Add tests.

--- grails-app/controllers/com/grailsinaction/UserController.groovy
+++ grails-app/controllers/com/grailsinaction/UserController.groovy
@@ -63,7 +63,47 @@ class UserController {
     }
 */
 
-    def index(Integer max) {
+    def register() {
+        if (request.method == "POST") {
+            def user = new User(params)
+            if (user.validate()) {
+                user.save()
+                flash.message = "Successfully Created User"
+                redirect(uri: '/')
+            } else {
+                flash.message = "Error Registering User"
+                return [ user: user ]
+            }
+        }
+    }
+
+    def register2(UserRegistrationCommand urc) {
+        if (urc.hasErrors()) {
+            render view: "register", model: [ user : urc ]
+        } else {
+            def user = new User(urc.properties)
+            user.profile = new Profile(urc.properties)
+            if (user.validate() && user.save()) {
+                flash.message = "Welcome aboard, ${urc.fullName ?: urc.loginId}"
+                redirect(uri: '/')
+            } else {
+                // maybe not unique loginId?
+                return [ user : urc ]
+            }
+        }
+    }
+
+    def profile(String id) {
+        def user = User.findByLoginId(id)
+        if (user) {
+            return [profile: user.profile]
+        } else {
+            response.sendError(404)
+        }
+    }
+
+
+def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond User.list(params), model:[userInstanceCount: User.count()]
     }
@@ -153,5 +193,32 @@ class UserController {
             }
             '*'{ render status: NOT_FOUND }
         }
+    }
+}
+
+class UserRegistrationCommand {
+    String loginId
+    String password
+    String passwordRepeat
+    byte[] photo
+    String fullName
+    String bio
+    String homepage
+    String email
+    String timezone
+    String country
+    String jabberAddress
+
+    static constraints = {
+        importFrom Profile
+        importFrom User
+        password(size: 6..8, blank: false,
+                validator: { passwd, urc ->
+                    return passwd != urc.loginId
+                })
+        passwordRepeat(nullable: false,
+                validator: { passwd2, urc ->
+                    return passwd2 == urc.password
+                })
     }
 }
